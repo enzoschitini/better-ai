2eb7f64b04291a1bac794534eab9b5d6760ecc06
enzoschitini <schitinienzo@mail.com>
2026-02-09 15:51:09 -0300

    Supabase Storage

--- src/image_generation/storage/supabase_storage.py
+++ src/image_generation/storage/supabase_storage.py
@@ -1,8 +1,8 @@
 import os
 from supabase import create_client, Client
 
-SUPABASE_URL = "https://xnuadageyjmxurcbduie.supabase.co"
-SUPABASE_KEY = "sb_secret_s_082ip6RyNkIvhd_bZfWA_k-lYWN4f"
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -17,12 +17,15 @@ def upload():
         )
 
     print(response)
-
+  
     public_url = supabase.storage.from_("images").get_public_url("uploads/mappa_20260204_202404_1.jpg")
     print(public_url)
 
 def delete():
-    response = supabase.storage.from_("images").remove([path])
+    response = supabase.storage.from_("images").remove([file_path])
 
     print(response)
 
+upload()
+
+# https://hsenyunovbrmjejxqvjn.supabase.co/storage/v1/object/public/images/uploads/mappa_20260204_202404_1.jpg
