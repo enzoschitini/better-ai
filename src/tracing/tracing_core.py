6c93892dd5e2436320d62a545ee7e9457c311a31
enzoschitini <schitinienzo@mail.com>
2026-02-21 17:57:29 -0300

    application_tracing

--- src/tracing/tracing_core.py
+++ src/tracing/tracing_core.py
@@ -1,105 +1,98 @@
-# tracing_core.py
-
-import logging
 import json
-from typing import Dict, Any
+from src.utils.unique_id_factory import IDGenerator
+import time
 
+#print(IDGenerator.timestamp(prefix="log_"))
 
-# =========================
-# LOGGER CONFIGURATION
-# =========================
-def setup_logger(name: str) -> logging.Logger:
-    logger = logging.getLogger(name)
+"""
 
-    if logger.handlers:
-        return logger  # evita duplicação de handlers
+"""
 
-    logger.setLevel(logging.DEBUG)
+class ApplicationTracing:
+    def __init__(self, 
+                 log_id: str = None, 
+                 flag: str = None, 
+                 file_name: str = None,
+                 save_logs: bool = False,
+                 show_informations_messages: bool = False,
+                 show_payloads: bool = False):
 
-    formatter = logging.Formatter(
-        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
-    )
+        self.log_id = log_id
+        self.flag = flag
+        self.file_name = file_name
+        self.save_logs = save_logs
+        self.show_informations_messages = show_informations_messages
+        self.show_payloads = show_payloads
+    
+    def INFO(self, 
+             func_name: str = None, 
+             message: str = None,
+             save_logs: bool = None,
+             show_informations_messages: bool = None,
+             show_payloads: bool = None):
 
-    # Handler arquivo
-    file_handler = logging.FileHandler("app.log")
-    file_handler.setFormatter(formatter)
-
-    # Handler console
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
-
-    return logger
-
-
-logger = setup_logger("TracingCore")
-
-
-# =========================
-# CORE FUNCTION
-# =========================
-def process_user_data(user: Dict[str, Any]) -> float | None:
-    logger.info("Start processing user", extra={"user": user})
-
-    try:
-        logger.debug(f"Payload received: {json.dumps(user)}")
-        logger.warning("d")
-
-        if "age" not in user:
-            logger.warning("Missing age field", extra={"user": user})
-
-        result = user["income"] / user["age"]
-
-        logger.info("Processing finished successfully", extra={"result": result})
-        return result
-
-    except KeyError as e:
-        logger.error(
-            "Missing required field",
-            extra={"error": str(e), "user": user},
-            exc_info=True
+        # fallback
+        save_logs = self.save_logs if save_logs is None else save_logs
+        show_informations_messages = (
+            self.show_informations_messages 
+            if show_informations_messages is None 
+            else show_informations_messages
+        )
+        show_payloads = (
+            self.show_payloads 
+            if show_payloads is None 
+            else show_payloads
         )
 
-    except Exception as e:
-        logger.critical(
-            "Unexpected error",
-            extra={"error": str(e), "user": user},
-            exc_info=True
+        print(
+            f"{time.time()} | INFO | {func_name}() | {message} | {self.file_name} | {self.log_id}"
         )
 
-    return None
 
 
-# =========================
-# ENTRYPOINT
-# =========================
-def main():
-    users = [
-        {"name": "Enzo", "age": 25, "income": 5000},
-        {"name": "Maria", "age": 10, "income": 3000},
-        {"name": "João", "age": 10, "income": 4000},
-    ]
-
-    for user in users:
-        process_user_data(user)
 
 
-if __name__ == "__main__":
-    main()
+
+logger = ApplicationTracing(log_id="log_1234", flag="Logging Test", file_name="tracing_core.py")
+logger.INFO("create_user", "User created")
 
 
 
 """
-import json
-from src.utils.unique_id_factory import IDGenerator
 
-print(IDGenerator.timestamp(prefix="log_"))
 
-class ApplicationTracing:
-    def __init__(self):
-        pass
+
+    def DEBUG(self, 
+             func_name:str = None, 
+             message:str = None,
+             save: bool = False):
+        print(
+            f"{time.time()} | INFO | {func_name}() | {message} | {self.file_name} | {self.log_id}"
+        )
+
+    def WARNING(self, 
+             func_name:str = None, 
+             message:str = None,
+             save: bool = False):
+        print(
+            f"{time.time()} | INFO | {func_name}() | {message} | {self.file_name} | {self.log_id}"
+        )
+
+    def ERROR(self, 
+             func_name:str = None, 
+             message:str = None,
+             save: bool = False):
+        print(
+            f"{time.time()} | INFO | {func_name}() | {message} | {self.file_name} | {self.log_id}"
+        )
+
+    def CRITICAL(self, 
+             func_name:str = None, 
+             message:str = None,
+             save: bool = False):
+        print(
+            f"{time.time()} | INFO | {func_name}() | {message} | {self.file_name} | {self.log_id}"
+        )
 """
 
 # python -m src.tracing.tracing_core
