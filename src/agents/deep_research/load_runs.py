fae12ba66f058152f3a9fd7bb1a67e3c3f1be56d
enzoschitini <schitinienzo@mail.com>
2026-03-12 08:12:31 -0300

    deep_research_agent

--- src/agents/deep_research/load_runs.py
+++ src/agents/deep_research/load_runs.py
@@ -5,7 +5,6 @@ conn = sqlite3.connect("src/agents/deep_research/agno.db")
 cursor = conn.cursor()
 
 cursor.execute("SELECT runs FROM agno_sessions")
-
 rows = cursor.fetchall()
 
 runs = []
@@ -14,14 +13,11 @@ for row in rows:
     raw = row[0]
 
     if raw:
-        data = json.loads(raw)  # converte string JSON -> dict
-        runs.append(data)
-
-#print(runs)
-data = json.loads(runs)
+        data = json.loads(raw)      # remove primeira camada
+        data = json.loads(data)     # remove segunda camada
+        runs.extend(data)           # junta na lista
 
 with open("src/agents/deep_research/response.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, indent=2, ensure_ascii=False)
+    json.dump(runs, f, indent=2, ensure_ascii=False)
 
-print(json.dumps(data, indent=2, ensure_ascii=False))
-
+print(json.dumps(runs, indent=2, ensure_ascii=False))
