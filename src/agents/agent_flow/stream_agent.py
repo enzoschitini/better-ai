68c117a6f2e13453242b20dd750b9e157225b3fc
enzoschitini <schitinienzo@mail.com>
2026-03-13 15:31:14 -0300

    Stream Response

--- src/agents/agent_flow/stream_agent.py
+++ src/agents/agent_flow/stream_agent.py
@@ -1,6 +1,32 @@
 import requests
 import json
 
+def get_agents():
+    response = requests.get("http://localhost:7777/agents").json()
+    print(response)
+
+    return response
+
+def simple_request():
+    agent_id = "fair-goodall-9e694aa8"
+    agent_id = "laura94"
+
+    url = f"http://localhost:7777/agents/{agent_id}/runs"
+
+    data = {
+        "message": "Explique o que é RAG",
+        "session_id": "session-1",
+        "stream": "false"
+    }
+
+    response = requests.post(url, data=data)
+    result = response.json()
+
+    #print(result)
+    print(f"\n\nResponse: {result["content"]}")
+
+
+
 agent_id = "laura94"
 url = f"http://localhost:7777/agents/{agent_id}/runs"
 
@@ -12,7 +38,7 @@ data = {
 
 response = requests.post(url, data=data, stream=True)
 
-def simple():
+def simple_stream():
     for line in response.iter_lines():
         if not line:
             continue
@@ -31,7 +57,7 @@ def simple():
             except:
                 pass
 
-def with_tools():
+def stream_with_tools():
     for line in response.iter_lines():
         if not line:
             continue
@@ -61,6 +87,12 @@ def with_tools():
             except:
                 pass
 
+def show_all():
+    for line in response.iter_lines():
+        if line:
+            decoded = line.decode("utf-8")
+            print(decoded)
+
 def save():
     stream_response = []  # variável que vai guardar tudo
 
@@ -86,3 +118,6 @@ def save():
 save()
 
 # Explique o que é RAG
+# http://localhost:7777/docs
+
+# python -m src.agents.agent_flow.stream_agent
