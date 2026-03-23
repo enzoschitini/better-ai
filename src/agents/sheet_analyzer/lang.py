d4039138cd1cef6bd1f8fa34dd40d53280b31f89
enzoschitini <schitinienzo@mail.com>
2026-03-23 12:37:33 -0300

    sheet_analyzer

---
+++ src/agents/sheet_analyzer/lang.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from langchain_openai import ChatOpenAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# 🔥 Carregar CSV
+df = pd.read_csv("src/agents/sheet_analyzer/supermarket_sales.csv")
+
+# 🔥 LLM
+llm = ChatOpenAI(
+    model="gpt-4.1",
+    temperature=0
+)
+
+# 🔥 Criar agente
+agent = create_pandas_dataframe_agent(
+    llm,
+    df,
+    verbose=True,
+    allow_dangerous_code=True
+)
+
+# 🔥 Pergunta
+resposta = agent.invoke("Qual produto teve mais vendas?")
+print(resposta)
--- src/agents/sheet_analyzer/toolkit.py
+++ src/agents/sheet_analyzer/toolkit.py
@@ -2,4 +2,6 @@
 
 
 
+
+
 # python -m src.agents.sheet_analyzer.toolkit
