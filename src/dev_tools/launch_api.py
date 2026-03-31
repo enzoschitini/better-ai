c9f4991dcefd25318fe398900718c117a53d29fc
EnzoSchitiniEcGlobal <essilva14@latam.stefanini.com>
2026-03-31 11:50:02 -0300

    Aggiungi la logica per il caricamento e l'analisi dei contenuti dei file nella route /parse-content, inclusi i dettagli sul file e il risultato dell'estrazione.

--- src/dev_tools/launch_api.py
+++ src/dev_tools/launch_api.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field
 from typing import List, Dict, Any, Optional, Literal
 from dotenv import load_dotenv
 
-from fastapi import FastAPI, UploadFile, HTTPException, Request, Form, Depends, Header, File
+from fastapi import FastAPI, UploadFile, HTTPException, Request, Form, Body, Depends, Header, File
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -50,6 +50,61 @@ def healthy():
 
 # uvicorn src.dev_tools.launch_api:app --reload 
 # uvicorn app:app --reload  
+
+# ------------------------------------------------- #
+from src.utils.load_file.load_request_file import LoadRequestFile
+from src.embedding.services.file_content_extractor import FileContentExtractor
+from src.text_parse.content_parsing_agent import ContentParsingAgent
+
+@app.post("/parse-content")
+async def parse_content(
+    schema: str = Form(...),
+    file: UploadFile = File(...),
+):
+    try:
+
+        loader = await LoadRequestFile(
+            file=file,
+            max_size_mb=5
+        ).load()
+
+        schema_data = json.loads(schema)
+
+        file_bytes = loader.bytes
+        raw_bytes = file_bytes.getvalue()
+        file_extension = loader.extension
+
+        print(type(file_bytes))  # BytesIO
+        print(file_extension)    # extensão do arquivo
+        print(len(raw_bytes))    # tamanho em bytes
+        print(raw_bytes[:100])   # primeiros 100 bytes do arquivo
+
+        extractor = FileContentExtractor(file_bytes, "pdf")
+        result = extractor.extract()
+        print(result["response"][:500])  # Imprime os primeiros 500 caracteres do conteúdo extraído
+
+        agent_parser = ContentParsingAgent(
+            input_data={
+                "file_content": result["response"]
+            },
+            output_data={
+                "summary": {
+                    "type": "str",
+                    "description": "Resumo do conteúdo do arquivo"
+                }
+            },
+            #config_data=config_data
+        )
+        content_parsed = agent_parser.run_agent()
+        response = agent_parser.format_response(content_parsed)
+
+        return JSONResponse(content={
+            "status": "success",
+            "response": response
+        })
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 
