7854c8d102978c008f5fd7140e100452199ec625
EnzoSchitiniEcGlobal <essilva14@latam.stefanini.com>
2026-04-14 18:19:19 -0300

    Aggiungi commento per la creazione di una tabella con percentuali di risposta per classe

--- src/dataframe_analyzers/pd_df_agent/agent.py
+++ src/dataframe_analyzers/pd_df_agent/agent.py
@@ -333,6 +333,7 @@ if __name__ == "__main__":
             "Quais generos foram representados na pesquisa?"
         )
 
+# Monte uma tabela relacionado resposta e classe. Com o percentual de resposta de cada classe para cada resposta
 # Crie uma tabela da média de idade por resposta
 # Qual a média de idade por resposta?
 # Quais as 5 principais respostas da pesquisa?
