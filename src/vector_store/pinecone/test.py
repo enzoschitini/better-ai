e9ce30a991b96ead685a560ec4e8f1fff15f6e2c
enzoschitini <schitinienzo@mail.com>
2026-04-19 10:28:48 -0300

    Dataframe Agent VS

--- src/vector_store/pinecone/test.py
+++ src/vector_store/pinecone/test.py
@@ -18,14 +18,14 @@ class TestPineconeVectorStore:
         return pine_client
 
     def retriver(
-            self, 
-            query: str = "Quais arquivos estão na base?",
-            filter_search: dict = {
-                "file_id": [
-                    "21d75dca2eec7b02080327f40220e20dxx2"
-                ]
-            },
-            k: int = 5
+        self, 
+        query: str = "Quais arquivos estão na base?",
+        filter_search: dict = {
+            "file_id": [
+                "21d75dca2eec7b02080327f40220e20dxx2"
+            ]
+        },
+        k: int = 5
         ):
 
         retriver = PineconeRetriever()
@@ -40,16 +40,19 @@ class TestPineconeVectorStore:
 
         return result
 
-    def embedding(self):
+    def embedding(
+        self,
+        embedding_content: str = None,
+        embedding_metadata: dict = {"user_id": "user_1234567", "source": "embedding_test.py"}
+    ):
         pine_service = PineconeVectorService(embedding_model_name="text-embedding-3-large", dimensions=3072)
 
-        embedding_content = """
-    Embeddings are vector representations of data (such as text, documents, or images) that capture their semantic meaning in a numerical space.
-    This technique allows for the comparison of content by similarity, enabling semantic searches, classification, recommendation, and information retrieval.
-    By transforming unstructured data into vectors, embeddings make it possible to efficiently index and query large volumes of information in vector databases.
-    """
-
-        embedding_metadata = {"user_id": "user_1234567", "source": "embedding_test.py"}
+        if embedding_content == None:
+            embedding_content = """
+        Embeddings are vector representations of data (such as text, documents, or images) that capture their semantic meaning in a numerical space.
+        This technique allows for the comparison of content by similarity, enabling semantic searches, classification, recommendation, and information retrieval.
+        By transforming unstructured data into vectors, embeddings make it possible to efficiently index and query large volumes of information in vector databases.
+        """
 
         response = pine_service.generate_vectors(
             text=str(embedding_content),
@@ -62,10 +65,15 @@ class TestPineconeVectorStore:
 
         return response
 
-    def delete(self):
+    def delete(
+        self,
+        target_feature: str = "source",
+        target_id: str = "embedding_test.py",
+        namespace: str = "betterai-embeddings-dev"
+    ):
         pine_service = PineconeVectorService(embedding_model_name="text-embedding-3-large", dimensions=3072)
 
-        delete = pine_service.delete_documents("source", "embedding_test.py", "betterai-embeddings-dev")
+        delete = pine_service.delete_documents(target_feature, target_id, namespace)
         print(f"\n{delete}\n")
 
         return delete
