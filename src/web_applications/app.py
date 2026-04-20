ee33c2be57191b14586aa5b58b1d346465162988
enzoschitini <schitinienzo@mail.com>
2026-04-20 08:54:34 -0300

    Base

--- src/web_applications/app.py
+++ src/web_applications/app.py
@@ -3,4 +3,16 @@ import streamlit as st
 st.set_page_config(page_title="BetterAI", page_icon="AI")
 st.image("images/Frame 27346.png")
 
+with st.sidebar:
+    context = st.selectbox(
+        label="",
+        options=["Applications", "Documentation"],
+    )
+
+    if context == "Applications":
+        st.write("Applications")
+
+    elif context == "Documentation":
+        st.write("Documentation")
+
 # streamlit run src/web_applications/app.py
