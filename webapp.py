215f1999d161e2e2558a36aa97fd79393d05335f
enzoschitini <schitinienzo@mail.com>
2026-04-20 15:01:12 -0300

    Web App

--- webapp.py
+++ webapp.py
@@ -1,7 +1,7 @@
+import importlib
 import streamlit as st
 
 from src.web_applications.config import PAGES
-from src.web_applications.applications.home import Home
 
 st.set_page_config(page_title="BetterAI", page_icon="AI")
 
@@ -23,9 +23,13 @@ with st.sidebar:
 if st.session_state.page is None:
     st.session_state.page = PAGES[context][0]
 
-if st.session_state.page == "Home":
-    page = Home()
+page_name = st.session_state.page.lower()  # ex: "Home" → "home"
+module_path = f"src.web_applications.applications.{page_name}"
 
+module = importlib.import_module(module_path)
+page_class = getattr(module, st.session_state.page)
+
+page = page_class()
 page.run()
 
 # streamlit run webapp.py
