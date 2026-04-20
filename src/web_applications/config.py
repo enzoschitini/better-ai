57e0c32d253146e5573c48d2449bd4ead44fd8ee
enzoschitini <schitinienzo@mail.com>
2026-04-20 14:52:16 -0300

    Web App

---
+++ src/web_applications/config.py
@@ -0,0 +1,11 @@
+PAGES = {
+    "Applications": [
+        "Home", 
+        "Text to Aquarela", 
+        "Image to Aquarela"
+    ],
+    "Documentation": [
+        "API", 
+        "Tutoriais"
+    ]
+}
--- webapp.py
+++ webapp.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
+from src.web_applications.config import PAGES
 from src.web_applications.applications.home import Home
 
 st.set_page_config(page_title="BetterAI", page_icon="AI")
@@ -7,32 +9,23 @@ st.set_page_config(page_title="BetterAI", page_icon="AI")
 if "page" not in st.session_state:
     st.session_state.page = None
 
-# Configuração das páginas
-PAGES = {
-    "Applications": ["Home", "Text to Aquarela", "Image to Aquarela"],
-    "Documentation": ["API", "Tutoriais"]
-}
-
 with st.sidebar:
-    # Contexto (selectbox sem label)
     context = st.selectbox(
         "",
-        ["Applications", "Documentation"],
+        PAGES.keys(),
         label_visibility="collapsed"
     )
 
-    # Botões das páginas
     for page in PAGES[context]:
         if st.button(page, use_container_width=True):
             st.session_state.page = page
 
-# Página padrão (caso nenhuma tenha sido clicada ainda)
 if st.session_state.page is None:
     st.session_state.page = PAGES[context][0]
 
-# --- Render ---
 if st.session_state.page == "Home":
-    home = Home()
-    home.run()
+    page = Home()
 
-# streamlit run src/web_applications/app.py
+page.run()
+
+# streamlit run webapp.py
