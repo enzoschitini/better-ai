4be9558fb5bf805e81d525b30747d7c8a7e622bd
enzoschitini <schitinienzo@mail.com>
2026-05-10 20:01:12 -0300

    DeleteEmbeddings

--- src/embedding/applications.py
+++ src/embedding/applications.py
@@ -1,5 +1,7 @@
 from src.embedding.modules.embedding_file import EmbeddingFile
+from src.embedding.modules.delete_embeddings import DeleteEmbeddings
 
 __all__ = [
-    "EmbeddingFile"
+    "EmbeddingFile",
+    "DeleteEmbeddings"
 ]
