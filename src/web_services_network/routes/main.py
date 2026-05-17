9f0478b227ddde234a57c5c2a35c211fb3dbeaca
enzoschitini <schitinienzo@mail.com>
2026-05-17 15:35:35 -0300

    feat: Aggiungi endpoint per il parsing dei documenti e definisci il modello di richiesta

--- src/web_services_network/routes/main.py
+++ src/web_services_network/routes/main.py
@@ -1,10 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+
+from src.web_services_network.auth import Authorization
 
 router = APIRouter(
-    #prefix="/",
-    tags=["embedding"]
+    tags=["parses"]
 )
 
-@router.post("/embedding")
-def send_message():
-    return {"message": "Mensagem enviada"}
+class FakeParse:
+    def __init__(self):
+        pass
+
+    def run(self):
+        return {"message": "Documento processado"}
+
+class DocumentParseRequest(BaseModel):
+    text: str
+    model: str
+    max_pages: int
+
+@router.post(
+    "/document-parse",
+    summary="Parse a document",
+    #dependencies=[Depends(Authorization.validate_api_key)],
+)
+def parse_document(body: DocumentParseRequest):
+    try:
+        erro = 1 / 0
+        parser = FakeParse()
+        response = parser.run()
+        return response
+
+    except Exception as e:
+        return {"error": str(e)}
