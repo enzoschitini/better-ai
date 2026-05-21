b40632203e264ddf5ed5a8a76a00c26a04d412bc
EnzoSchitiniEcGlobal <essilva14@latam.stefanini.com>
2026-05-21 17:29:03 -0300

    feat: riformatta la raccolta delle route per migliorare la leggibilità

--- web_services.py
+++ web_services.py
@@ -4,15 +4,11 @@ web_service = WebServiceAPI()
 
 app = web_service.initialize()
 
-ROUTES = web_service.collect_routers("src.web_services_network.routes")
+ROUTES = web_service.collect_routers(
+    "src.web_services_network.routes"
+)
 
 web_service.include_routers(ROUTES)
-
-
-# 1. Tipagem dos endpoints para facilitar a integração com outras linguagens e sistemas
-# 2. Implementar suporte para documentação automática dos endpoints
-# 3. Documentar os endpoints usando docstrings e as classes
-# 4. Revisar o módulo
 
 
 # uvicorn web_services:app --reload
