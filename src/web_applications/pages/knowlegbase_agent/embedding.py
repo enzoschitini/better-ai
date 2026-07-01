424ac63a192ce531521278e81c83bad4002682e5
Enzo Schitini <essilva14@latam.stefanini.com>
2026-07-01 15:35:37 -0300

    Refactor FileProcessor to enhance file loading and embedding payload generation

--- src/web_applications/pages/knowlegbase_agent/embedding.py
+++ src/web_applications/pages/knowlegbase_agent/embedding.py
@@ -1,4 +1,11 @@
+from io import BytesIO
+
 import streamlit as st
+
+import json
+
+from io import BytesIO
+from src.embedding.modules.embedding_file import EmbeddingFile
 
 def embedding():
     with st.sidebar:
@@ -6,79 +13,98 @@ def embedding():
     
 
 
+class FileProcessor:
+    def __init__(self):
+        pass
 
+    def _load_file(self, path):
+        # Implement the logic to load and process the file
+        with open(path, "rb") as f:
+            file_bytes = BytesIO(f.read())
+        return file_bytes
 
-
-
-import json
-
-from io import BytesIO
-from src.embedding.modules.embedding_file import EmbeddingFile
-
-def generate_payload(view: bool = False):
-    with open("Credencial Sesc.pdf", "rb") as f:
-        file_bytes = BytesIO(f.read())
-    
-    if view:
-        file_bytes = file_bytes[:20]
-
-    payload = {
-        "job_id": "job_12345",
-
-        "identifiers": {
-            "client_id": "client_abc",
-            "workspace_id": "workspace_001",
-            "user_id": "user_789",
-            "file_id": "file_xyz" # Può essere creato
-        },
-
-        "pipeline": {
-            "generate_tags": True,
-        },
-
-        "embedding_metadata": {
-            "source": "uploaded_file",
-            "origin": "web_app",
-            "language": "en",
-            "tags": "#finance, #report, #2026"
-        },
-
-        "embedding_settings": {
-            "model": "text-embedding-3-large",
-            "dimensions": 3072,
-            "chunk_size": 500,
-            "chunk_overlap": 50,
-            "normalize": True,
-            "batch_size": 200,
-        },
-
-        "vector_db_settings": {
-            "save_global": False,
-            "main_namespace": "default_main_namespace",
-        },
-
-        "file_info": {
+    def get_file_information(self, file):
+        # Implement the logic to extract file information
+        return {
             "name": "example.pdf",
             "extension": "pdf",
             "mime_type": "application/pdf",
             "size_bytes": 204800,
             "size_kb": 200,
             "size_mb": 0.2,
-            "bytes": file_bytes
+            "bytes": file
         }
-    }
 
-    if view:
-        print(json.dumps(payload, indent=4, default=str))
+    def build_embedding_payload(
+        self, 
+        job_id: str,
+        user_id: str,
+        source: str
+    ):
 
-    return payload
+        payload = {
+            "job_id": "job_12345",
+
+            "identifiers": {
+                "client_id": "client_abc",
+                "workspace_id": "workspace_001",
+                "user_id": "user_789",
+                "file_id": "file_xyz" # Può essere creato
+            },
+
+            "pipeline": {
+                "generate_tags": True,
+            },
+
+            "embedding_metadata": {
+                "source": "uploaded_file",
+                "origin": "web_app",
+                "language": "en",
+                "tags": "#finance, #report, #2026"
+            },
+
+            "embedding_settings": {
+                "model": "text-embedding-3-large",
+                "dimensions": 3072,
+                "chunk_size": 500,
+                "chunk_overlap": 50,
+                "normalize": True,
+                "batch_size": 200,
+            },
+
+            "vector_db_settings": {
+                "save_global": False,
+                "main_namespace": "default_main_namespace",
+            },
+
+            "file_info": {
+                "name": "example.pdf",
+                "extension": "pdf",
+                "mime_type": "application/pdf",
+                "size_bytes": 204800,
+                "size_kb": 200,
+                "size_mb": 0.2,
+                "bytes": self._load_file("Credencial Sesc.pdf")
+            }
+        }
+
+        return payload
+
+
+    def embedding_file(self, payload):
+        embedder = EmbeddingFile(payload)
+        embedder._init_tracking()
+        embedder.run()
+        embedder.save()
+
 
 if __name__ == "__main__":
-    payload = generate_payload()
+    embedding_processor = FileProcessor()
+    payload = embedding_processor.build_embedding_payload(
+        job_id="job_12345",
+        user_id="user_789",
+        source="uploaded_file"
+    )
+    #embedding_processor.embedding_file(payload)
 
-    embedder = EmbeddingFile(payload)
-    embedder._init_tracking()
-    embedder.run()
-    embedder.save()
-
-# python -m src.embedding.test.embbedding_file
+# python -m src.web_applications.pages.knowlegbase_agent.embedding
